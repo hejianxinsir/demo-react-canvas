327eccbb66a4be4d67de7e1bd61956668c288512
hejianxin <hejianxinsir@gmail.com>
2019-12-06 15:55:39 +0800

    可以click打点了，获取context成功

--- src/Canvas.js
+++ src/Canvas.js
@@ -5,19 +5,18 @@ import './Canvas.css';
 class Canvas extends React.Component{
 	constructor(props){
 		super(props)
-		this.myRef = React.createRef();
 		this.state = {
 			canvasWidth: window.innerWidth,
 			canvasHeight: window.innerHeight 
 		}
 		this.updateDimensions = this.updateDimensions.bind(this);
 		this.dot = this.dot.bind(this);
+		this.drawLine = this.drawLine.bind(this);
 	}
 
 	conponentDidMount(){
 		window.addEventListener("resize", this.updateDimensions);
-		const canvas = this.props.ref;
-		const context = canvas.getContext('2d');
+		this.dot();
 	}
 	
 	updateDimensions(){
@@ -27,19 +26,33 @@ class Canvas extends React.Component{
 		})
 	}
 
-	dot(){
-			context.beginPath();
-			context.arc(100,100,10,0,Math.PI*2)
-			context.stroke();
+	dot(aaa){
+		const context = this.refs.canvas.getContext('2d');
+		let {clientX,clientY} = aaa;
+		let previousDot = {x:clientX, y: clientY};
+
+		context.beginPath();
+		context.arc(clientX,clientY,1,0,Math.PI*2);
+		context.fill();
 	}
-	
+
+	drawLine(aaa){
+		const context = this.refs.canvas.getContext('2d');
+		let {x,y} = aaa;
+		let presentDot = {x: x, y: y};
+		context.beginPath();
+		context.moveTo(x1,y1);
+		context.lineTo(x2,y2);
+		context.stroke();
+	}
 
 	render(){
 		return(
-				<canvas ref={this.myRef.current}
+				<canvas ref="canvas"
 						width={this.state.canvasWidth}
 						height={this.state.canvasHeight}
-						onClick = {this.dot}
+						onMouseDown={this.dot}
+						onMouseMove={this.drawLine}
 				></canvas>	
 		)
 	}
